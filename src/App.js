ed308e568b23ccc432a01c10665fe0b023784bf7
BrianLevin <bml201095@gmail.com>
2021-03-09 17:36:09 -0500

    turned the standard functional based component in the app.js file to a class based compononant

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React,{Component} from "react";
 
 import './App.css';
 
-function App() {
+class App  extends Component{
+
+  render() { // render is a method within a class life cycle method and runs at a certain point when the components are loaded and then it renders
+
+
   return (
     <div className="App">
      <h1>Hello from React</h1>
     </div>
   );
+}
 }
 
 export default App;
