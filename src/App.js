1c1e79f0ef3452e71d0d375efa000f1bf19cd7ff
BrianLevin <bml201095@gmail.com>
2021-03-10 11:56:45 -0500

    added the icon prop

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App  extends Component{
 
   return (
     <div className = 'App'>
-     <Navbar title = " Github Finder" /> // nav bar prpp
+     <Navbar title = " Github Finder" icon = 'fab fa-github'  /> 
      
     </div>
   );
