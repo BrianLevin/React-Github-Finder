daadb7b070909c13501f05341c51886022af03d4
BrianLevin <bml201095@gmail.com>
2021-03-10 14:45:29 -0500

    created this state object with values

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react'
    class UserItem extends Component {
 
     constructor () { // adding state to a class based component using a constructor function thats going to run when the component runs
-        super();
+        super();// call super in order to call the parent class constructor
 
-        this.state { // state is just a javascript object
+        this.state  = { // state is just a javascript object
+         id: 'id',
+         login: 'mojombo',
+         avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+         html_url: 'https://github.com/mojombo'
+
+
 
 
         }
