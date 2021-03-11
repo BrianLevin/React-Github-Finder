59e484725c88bade5e91b940b5cd2d104edbe212
BrianLevin <bml201095@gmail.com>
2021-03-11 18:36:10 -0500

    removed state from the UserItem since  we are getting data from the objects props now

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -24,11 +24,11 @@ import React, { Component } from 'react'
 
     render() {
 
-const {login, avatar_url, html_url } = this.props.user // destructored this.props.user to hget values
+const {login, avatar_url, html_url } = this.props.user; // destructored this.props.user to hget values
 
         return  (
             <div className = "card text-center">
-            <img src = {this.state.avatar_url}  alt= "" className = "round-img" style= {{width: '60px'}} />   {/*  grab somthing from state within a class also included some  globall css class and added inline style */}
+            <img src = {avatar_url}  alt= "" className = "round-img" style= {{width: '60px'}} />   {/*  grab somthing from state within a class also included some  globall css class and added inline style */}
             <h3>{login}</h3> {/* shows login name */}
             <div>
             <a href = {html_url} className= "btn btn-dark btn-sm my-1"> More
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -6,7 +6,7 @@ state= {
 users: [ {
     id: '1',
     login: 'mojombo',
-    avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
     html_url: 'https://github.com/mojombo'
    
 
@@ -29,7 +29,7 @@ users: [ {
     id: '3',
     login: 'pjhyett',
     avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-    html_url: 'https://github.com/mojombo'
+    html_url: 'https://github.com/pjhyett'
 
 
 
@@ -40,13 +40,13 @@ users: [ {
 ]
 
 
-}
+};
 
     render() {
         return (
             <div>
                 {this.state.users.map(user => (   // pass in the whole user for all object values then pass as prop to user item
-                    <UserItem key= {user.id}>user={user}</UserItem>  // used key to break organize code more efficently and prevent warnings in the console
+                    <UserItem key= {user.id} user={user}/>  // used key to break organize code more efficently and prevent warnings in the console
                 ))} {/*  a high order array method that takes in a function and will use a call a call back function to get data */}
             </div>
         )
