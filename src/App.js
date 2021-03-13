cfbdfd1f68917e93e14e2e4609e44dbcc6aaeb74
BrianLevin <bml201095@gmail.com>
2021-03-12 21:41:21 -0500

    added the spinner component in the js file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar"; // exported from navbar class
 import Users from "./components/users/Users";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-// put res.data users that come back into state
+  // put res.data users that come back into state
   state = {
-
-users: [],
-loading: false
-
-
-  }
+    users: [],
+    loading: false,
+  };
 
   //life cycle method get users data with a promose
-   async componentDidMount(){
-     //loading
-this.setState({loading: true});
+  async componentDidMount() {
+    //loading
+    this.setState({ loading: true });
 
+    const res = await axios.get("https://api.github.com/users");
+    // get the data from user, then set loading back to false
+    this.setState({ user: res.data, loading: false });
 
-
-     const res= await axios.get('https://api.github.com/users')
-  // get the data from user, then set loading back to false
-this.setState({ user: res.data,loading: false });
-
-  console.log(res.data);
-
+    console.log(res.data);
   }
 
-
   render() {
-
     // render is a method within a class life cycle method and runs at a certain point when the components are loaded and then it renders
     //const numbers= [1,2,3];
 
@@ -39,13 +31,10 @@ this.setState({ user: res.data,loading: false });
       <div className="App">
         <Navbar />
 
-        <div className = "container">
-
-        {/*  passed down users  state components to props */}
-        <Users  loading= {this.state.loading} users= {this.state.users}/>
+        <div className="container">
+          {/*  passed down users  state components to props */}
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
-        {/* nav bar props get passed here or they can be overwritten here if you type directly in the navbar */}
-        
       </div>
     );
   }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,51 +2,23 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-
-      {
-        id: "2",
-        login: "defunkt",
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-
-      {
-        id: "3",
-        login: "pjhyett",
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
-  };
-
   render() {
     return (
-      <div style= {userStyle}>
-        {this.state.users.map((
-          user // pass in the whole user for all object values then pass as prop to user item
-        ) => (
+      <div style={userStyle}>
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} /> // used key to break organize code more efficently and prevent warnings in the console
-        ))}{" "}
-        {/*  a high order array method that takes in a function and will use a call a call back function to get data */}
+        ))}
+        
       </div>
     );
   }
 }
 
-const userStyle= {
-display: 'grid',
-gridTemplateColumns: 'repeat(3, 1fr)',
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
 
-gridGap: '1rem'
-
-}
+  gridGap: "1rem",
+};
 
 export default Users;
