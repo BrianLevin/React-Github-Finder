2b7b8098d4c5c2116c61f3523176f3a0136767ca
BrianLevin <bml201095@gmail.com>
2021-03-12 22:01:03 -0500

    added a spinner if its still loading

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
 const Users = ({users, loading}) => {
+    if(loading) {
+
+return <Spinner />
+    } else {
+
+        return (
+            <div style={userStyle}>
+              {users.map(user => (
+                <UserItem key={user.id} user={user} /> // used key to break organize code more efficently and prevent warnings in the console
+              ))}
+              
+            </div>
+          );
+    }
   
-    return (
-      <div style={userStyle}>
-        {users.map(user => (
-          <UserItem key={user.id} user={user} /> // used key to break organize code more efficently and prevent warnings in the console
-        ))}
-        
-      </div>
-    );
+   
   
 }
 
