cdac249fbc4e432dcd7dfc0b3a56827fd85bd7b8
BrianLevin <bml201095@gmail.com>
2021-05-24 00:58:33 -0400

    created nav bar links that do not change the state once the user navigates pages

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types'; // declare prop types prop checiking
-
+import React from "react";
+import PropTypes from "prop-types"; // declare prop types prop checiking
+import{ Link } from 'react-router-dom';
 
 // destructured icon and title
 
-const Navbar=({icon,title}) => {
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="  navbar bg-primary">
+      <h1>
+        {" "}
+        <i className={icon} /> {title} {/* render the props  */}
+      </h1>
+      <ul>
+        <li><Link to ='/'>Home</Link>
+        </li>
+        <li>
+<Link to = '/about'>About</Link>
 
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
+Navbar.defaultProps = {
+  /* pass default  props in object  */ title: "Github Finder",
+  icon: "fab fa-github",
+};
 
+Navbar.propTypes = {
+  // prop checking
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
-    
-    
-        return (
-            <nav className= "  navbar bg-primary">
-                <h1> <i className= {icon} /> {title} {/* render the props  */}
-               
-               </h1>
-            </nav>
-        )
-}
-
-Navbar.defaultProps = {  /* pass default  props in object  */
-    title: 'Github Finder',
-    icon: 'fab fa-github'
-    
-        }
-    
-        Navbar.propTypes = { // prop checking
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-        };
-
-export default Navbar
+export default Navbar;
